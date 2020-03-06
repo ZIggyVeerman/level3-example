2f1d5d7bf086754240e61a304e9ee82e76e6c539
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-03-06 14:03:31 +0100

    example2 code added

--- app/src/main/java/com/example/reminderapp/MainActivity.kt
+++ app/src/main/java/com/example/reminderapp/MainActivity.kt
@@ -5,21 +5,86 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.reminderapp.R
+import com.example.reminderapp.Reminder
+import com.example.reminderapp.ReminderAdapter
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+  private val reminders = arrayListOf<Reminder>()
+  private val reminderAdapter =
+    ReminderAdapter(reminders)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(toolbar)
 
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show()
+    initViews()
+
+    fab.setOnClickListener {
+      val reminder = etReminder.text.toString()
+      addReminder(reminder)
+
     }
   }
+
+  private fun initViews() {
+    // Initialize the recycler view with a linear layout manager, adapter
+    rvReminders.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+    rvReminders.adapter = reminderAdapter
+    rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+    createItemTouchHelper().attachToRecyclerView(rvReminders)
+
+  }
+
+  private fun addReminder(reminder: String) {
+    if (reminder.isNotBlank()) {
+      reminders.add(Reminder(reminder))
+      reminderAdapter.notifyDataSetChanged()
+      etReminder.text?.clear()
+    } else {
+      Snackbar.make(etReminder, "You must fill in the input field!", Snackbar.LENGTH_SHORT).show()
+    }
+  }
+
+  /**
+   * Create a touch helper to recognize when a user swipes an item from a recycler view.
+   * An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+   * and uses callbacks to signal when a user is performing these actions.
+   */
+  private fun createItemTouchHelper(): ItemTouchHelper {
+
+    // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+    // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+    val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+      // Enables or Disables the ability to move items up and down.
+      override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+      ): Boolean {
+        return false
+      }
+
+      // Callback triggered when a user swiped an item.
+      override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        val position = viewHolder.adapterPosition
+        reminders.removeAt(position)
+        reminderAdapter.notifyDataSetChanged()
+      }
+    }
+    return ItemTouchHelper(callback)
+  }
+
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
     // Inflate the menu; this adds items to the action bar if it is present.
